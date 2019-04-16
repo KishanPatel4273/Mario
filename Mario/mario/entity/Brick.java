3e54bc26e2dcb7700846fdc94ad179d5e0d4d42b
kelli <kelli@DESKTOP-U53MJVP>
2019-04-16 15:16:30 -0400

    he

--- Mario/mario/entity/Brick.java
+++ Mario/mario/entity/Brick.java
@@ -3,7 +3,7 @@ package entity;
 import loaders.TextureLoader;
 import objects.Entity;
 
-public class Brick extends Entity {
+public class Brick extends Entity{
 
 	public Brick(int x, int y, int width, int height, String type) {
 		super(x, y, width, height, TextureLoader.brownBrick);
