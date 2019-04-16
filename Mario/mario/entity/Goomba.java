16b81a51e57fd2a81df03cca4ce3e50c123388a2
kelli miklinevich <kelli.gowdy@gmail.com>
2019-04-16 15:48:09 -0400

    hello

--- Mario/mario/entity/Goomba.java
+++ Mario/mario/entity/Goomba.java
@@ -1,5 +1,34 @@
 package entity;
 
-public class Goomba {
+import objects.Entity;
 
+public class Goomba extends Entity{
+	private int direction;
+	private int speed;
+
+	public Goomba(int x, int y, int width, int height, int id) {
+		super(x, y, width, height, id);
+		direction=-1;
+		speed=1;
+	}
+
+	public void collide(String direction) {
+		if(direction.equals("-1")) {
+			this.direction=1;
+		}
+		if(direction.equals("1")) {
+			this.direction=-1;
+		}
+	}
+	
+	public void update() {
+		if(direction==1) {
+			addX(speed);
+		}
+		if(direction==-1) {
+			addX(-speed);
+		}
+	}
+	
+	
 }
